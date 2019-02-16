6e5f59425056e3dc33ab35b85f3a3c314f64360a
Roy Gero <roy.gero.ii@gmail.com>
2019-02-15 21:53:03 -0500

    Starting to parse the query.

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import moment from 'moment';
 
 //Components
 import Header from './Header';
@@ -11,9 +12,46 @@ export class ResultsPage extends Component {
       if (!props.query){
         this.props.history.push('/')
       }
+      this.generateURL = this.generateURL.bind(this);
+  }
+
+  generateURL(params){
+    var url = "http://localhost:8888/api?"
+    if(!params) { return; }
+    console.log(params);
+    // Parse Season
+    if (params["season"]) { url = url + "season=" + params["season"]}
+
+    // Parse Players
+    if (params["players"])
+    {
+      url = url + "&players=" + params["players"]
+    }
+
+    // Parse Penalty
+    if (params["penalty"])
+    {
+      url = url + "&penalty=" + params["penalty"]
+    }
+
+    if (params["opponents"])
+    {
+      url = url + "&opponents=" + params["opponents"].join()
+    }
+    if (params["teams"])
+    {
+      url = url + "&teams=" + params["teams"].join()
+    }
+
+
+
+
+    return url;
   }
 
   render() {
+    var url = this.generateURL(this.props.query);
+    console.log(url);
     return (
       <div>
         <Header/>
@@ -25,7 +63,7 @@ export class ResultsPage extends Component {
 
 const mapStateToProps = (state, props) => {
     return {
-        query: state.query,
+        ...state.query,
         results: null
     };
 }
