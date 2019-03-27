bfa33b465561f60b3bf716d32129a6f2edf074a1
Roy Gero <roy.gero.ii@gmail.com>
2019-03-27 13:32:31 -0400

    Getting rid of a variable I don't need or use.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-export const Header = ({ startLogout }) => (
+export const Header = () => (
     <header className="header">
         <div className="header__content">
             <Link className="header__title" to="/" exact="true"><h1>The Penalty Tracker</h1></Link>
